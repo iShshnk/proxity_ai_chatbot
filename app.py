bcf15c279ee8244c7355abec980a5ab0acdeb55c
mayank-kr <kmayank38@gmail.com>
2023-06-22 20:06:55 +0530

    minor change

--- app.py
+++ app.py
@@ -254,5 +254,5 @@ app.jinja_env.globals.update(_build_auth_code_flow=_build_auth_code_flow)  # Use
 
 # run the Flask app in debug mode
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host = '0.0.0.0', port = 5000)
 
