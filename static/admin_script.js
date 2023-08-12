9833ef5b44b654cfa00094da5b94d5a14d040f3c
Shashank Ramesh <73171829+iShshnk@users.noreply.github.com>
2023-08-12 09:35:31 +0530

    ui update

--- static/admin_script.js
+++ static/admin_script.js
@@ -10,10 +10,6 @@ allSideMenu.forEach(item=> {
 		li.classList.add('active');
 	})
 });
-
-
-
-
 // TOGGLE SIDEBAR
 const menuBar = document.querySelector('#content nav .bx.bx-menu');
 const sidebar = document.getElementById('sidebar');
@@ -21,13 +17,6 @@ const sidebar = document.getElementById('sidebar');
 menuBar.addEventListener('click', function () {
 	sidebar.classList.toggle('hide');
 })
-
-
-
-
-
-
-
 const searchButton = document.querySelector('#content nav form .form-input button');
 const searchButtonIcon = document.querySelector('#content nav form .form-input button .bx');
 const searchForm = document.querySelector('#content nav form');
@@ -43,19 +32,12 @@ searchButton.addEventListener('click', function (e) {
 		}
 	}
 })
-
-
-
-
-
 if(window.innerWidth < 768) {
 	sidebar.classList.add('hide');
 } else if(window.innerWidth > 576) {
 	searchButtonIcon.classList.replace('bx-x', 'bx-search');
 	searchForm.classList.remove('show');
 }
-
-
 window.addEventListener('resize', function () {
 	if(this.innerWidth > 576) {
 		searchButtonIcon.classList.replace('bx-x', 'bx-search');
