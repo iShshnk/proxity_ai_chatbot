735c5d079e53272cc283cd6fc72033e9f846f287
Khushi Rawat <77057510+Mystic-miracle@users.noreply.github.com>
2023-08-17 21:37:19 +0530

    Update admin_script.js

--- static/admin_script.js
+++ static/admin_script.js
@@ -3,7 +3,7 @@ const allSideMenu = document.querySelectorAll('#sidebar .side-menu.top li a');
 allSideMenu.forEach(item=> {
 	const li = item.parentElement;
 
-	item.addEventListener('click', function () {
+	item.addEventListener('mouseover', function () {
 		allSideMenu.forEach(i=> {
 			i.parentElement.classList.remove('active');
 		})
